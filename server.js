be3e5bf113d65200161bb894abb98600c12555a4
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-07-10 19:01:14 -0400

    connected controllers to server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const methodOverride = require('method-override');
-//const locationsController = require('/controllers/locationsController');
-//const petsController = require('/controllers/petsController');
+const locationsController = require('/controllers/locationsController');
+const petsController = require('/controllers/petsController');
 
 app.set('view engine', 'ejs');
 
@@ -13,8 +13,8 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use(express.urlencoded({extended: false}));
 
-//app.use('/locations', locationsController);
-//app.use('/pets', petsController);
+app.use('/locations', locationsController);
+app.use('/pets', petsController);
 
 
 //Routes--------------------
