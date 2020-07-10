7c2d36f75410804c6eee203e9d35c8a9b2cccb25
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-07-10 17:49:22 -0400

    Added boilerplate

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const methodOverride = require('method-override');
+//const locationsController = require('/controllers/locationsController');
+//const petsController = require('/controllers/petsController');
+
+app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use(express.urlencoded({extended: false}));
+
+//app.use('/locations', locationsController);
+//app.use('/pets', petsController);
+
+app.get('/', (req, res) => {
+    res.send('<h1>I am a Home Page</h1>');
+})
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page Not Found</h1>');
+})
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
