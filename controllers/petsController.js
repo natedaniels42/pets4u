efcdf6cfaba2d31fd7fb02ff33b71e7054031086
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-07-13 13:02:30 -0400

    Added new pet route

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const multer = require('multer');
+const upload = multer({dest: '../public/images'});
 
+router.get('/new', (req, res) => {
+    res.render('pets/new');
+});
 
 
 
