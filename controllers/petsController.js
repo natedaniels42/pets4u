ac117bd32f63fc62c4b246bd227c25ea949d1273
avyrie <pittman.ally@gmail.com>
2020-07-13 11:56:37 -0600

    locations index updated

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,84 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const multer = require('multer');
-const upload = multer({dest: '../public/images'});
 
-router.get('/new', (req, res) => {
-    res.render('pets/new');
-});
+const PETS = require('../data/petsList');
 
-router.post('/', (req, res) => {
-    db.Pet.create(req.body, (err, newPet) => {
-        if (err) console.log(err);
-        
-        console.log(newPet);
-        res.redirect('/pets')
-    })
-})
+//to put in server
+// app.use('/pets', petsController);
 
-router.get('/', (req, res) => {
-    db.Pet.find({}, (err, foundPet) => {
-        if (err) console.log(err);
+//Routes-------------------------------
 
-        res.render('pets/index', {
-            pet: foundPet
-        });
-    });
-});
+//New Pet
 
-router.get('/confirm', (req, res) => {
-    
-    res.render('pets/confirm');
-})
+//Create Pet
 
-router.get('/:id', (req, res) => {
-    db.Pet.findById(req.params.id, (err, foundPet) => {
-        if (err) console.log(err);
+//Single Pet
 
-        res.render('pets/show', {
-            pet: foundPet
-        })
-    })
-})
+//All Pets
 
+//Edit One Pet
 
-router.get('/:id/adopt', (req, res) => {
-    db.Pet.findById(req.params.id, (err, foundPet) => {
-        if (err) console.log(err);
+//Update One Pet
 
-        res.render('pets/adopt', {
-            pet: foundPet,
-        });
-    })
-})
-
-router.get('/:id/edit', (req, res) => {
-    db.Pet.findById(req.params.id, (err, foundPet) => {
-        if (err) console.log(err);
-
-        res.render('pets/edit', {
-            pet: foundPet
-        })
-    })
-});
-
-router.put('/:id', (req, res) => {
-    db.Pet.findByIdAndUpdate(req.params.id, req.body, (err, updatedPet) => {
-        if (err) console.log(err);
-
-        console.log(req.params.id);
-        console.log(updatedPet);
-        res.redirect('/pets/' + req.params.id);
-    })
-})
-
-router.delete('/:id', (req, res) => {
-    db.Pet.findByIdAndDelete(req.params.id, (err, deletedPet) => {
-        if (err) console.log(err);
-
-        res.redirect('/pets');
-    })
-})
+//Delete One Pet
 
 module.exports = router;
