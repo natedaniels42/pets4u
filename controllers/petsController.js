3d955b034c1d7123d37ddb0d6027680e4de17063
avyrie <pittman.ally@gmail.com>
2020-07-13 12:34:28 -0600

    fixing

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const multer = require('multer');
+const upload = multer({dest: '../public/images'});
 
-const PETS = require('../data/petsList');
+router.get('/new', (req, res) => {
+    res.render('pets/new');
+});
 
-//to put in server
-// app.use('/pets', petsController);
+router.post('/', (req, res) => {
+    db.Pet.create(req.body, (err, newPet) => {
+        if (err) console.log(err);
+        
+        console.log(newPet);
+        res.redirect('/pets')
+    })
+})
 
+router.get('/', (req, res) => {
+    db.Pet.find({}, (err, foundPet) => {
+        if (err) console.log(err);
+
+        res.render('pets/index', {
+            pet: foundPet
+        });
+    });
+});
+
+router.get('/confirm', (req, res) => {
+    
+    res.render('pets/confirm');
+})
+
+router.get('/:id', (req, res) => {
+    db.Pet.findById(req.params.id, (err, foundPet) => {
+        if (err) console.log(err);
+
+        res.render('pets/show', {
+            pet: foundPet
+        })
+    })
+})
+
+
+router.get('/:id/adopt', (req, res) => {
+    db.Pet.findById(req.params.id, (err, foundPet) => {
+        if (err) console.log(err);
+
+        res.render('pets/adopt', {
+            pet: foundPet,
+        });
+    })
+})
 
 router.get('/:id/edit', (req, res) => {
     db.Pet.findById(req.params.id, (err, foundPet) => {
