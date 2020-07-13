0d4c5e634808b4ac01603add079bb38cb2329ddc
avyrie <pittman.ally@gmail.com>
2020-07-13 11:51:14 -0600

    added data to locations and pets lists, worked on controllers

---
+++ data/petsList.js
@@ -0,0 +1,235 @@
+const petList = [
+    {
+        name: 'Toby',
+        image:
+        location: 'Fairfax, VA',
+        sex: 'm',
+        age: '2 years old',
+        animalType: 'dog',
+        breedSpecies: 'boxer',
+        neutered: true,
+        description: 'Toby is a wonderful gentle giant with energy to spare. He is great with kids and adults alike. Good with cats and other dogs as well. He is housebroken and knows some basic commands. He is up to date on his bordatella and rabies vaccines.'
+    },
+    {
+        name: 'Squire',
+        image:
+        location: 'Lorton, VA',
+        sex: 'm',
+        age: '7 years old',
+        animalType: 'dog',
+        breedSpecies: 'pit bull',
+        neutered: true,
+        description: `Squire is everybody's best friend! He loves dogs, cats, birds, you name it! His favorite game is tag. He is very energetic and would do well in an active household. He is housebroken. He is up to date on his bordatella and rabies vaccines.`
+    },
+    {
+        name: 'Sera',
+        image:
+        location: 'Fredricksburg, VA',
+        sex: 'f',
+        age: '3 years old',
+        animalType: 'dog',
+        breedSpecies: 'lab mix',
+        neutered: true,
+        description: `Sera is a wonderful girl with a sweet disposition. She was rescued from a hoarding situation and is still working on her social skills. She is a bit nervous and would do best in a quiet household where she is the only dog. She is up to date on her bordatella and rabies vaccines.`
+    },
+    {
+        name: 'Sophie',
+        image:
+        location: 'Baltimore, MD',
+        sex: 'f',
+        age: '2 months old',
+        animalType: 'dog',
+        breedSpecies: 'cavalier king charles spaniel',
+        neutered: false,
+        description: `Sophie is an adorable bundle of joy who loves naps and cuddles. Being so young, she would do well in a household with older children and other dog-friendly pets. She is not yet housebroken and will need to finish her vaccination schedule.`
+    },
+    {
+        name: 'Ra',
+        image:
+        location: 'Annapolis, MD',
+        sex: 'm',
+        age: '2 years old',
+        animalType: 'dog',
+        breedSpecies: 'great dane',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'George',
+        image:
+        location: 'Fredrick, MD',
+        sex: 'm',
+        age: '5 years old',
+        animalType: 'dog',
+        breedSpecies: 'lab mix',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Henry',
+        image:
+        location: 'Philadelphia, PA',
+        sex: 'm',
+        age: '4 years old',
+        animalType: 'dog',
+        breedSpecies: 'english bulldog',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Sheila',
+        image:
+        location: 'Pittsburg, PA',
+        sex: 'f',
+        age: '2 years old',
+        animalType: 'dog',
+        breedSpecies: 'pit bull',
+        neutered: false,
+        description: 
+    },
+    {
+        name: 'Macie',
+        image:
+        location: 'Scranton, PA',
+        sex: 'f',
+        age: '1 year old',
+        animalType: 'dog',
+        breedSpecies: 'lab mix',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Mr. Mouse',
+        image:
+        location: 'Newark, DE',
+        sex: 'm',
+        age: '2 years old',
+        animalType: 'cat',
+        breedSpecies: 'domestic shorthair',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Sophie',
+        image:
+        location: 'Dover, DE',
+        sex: 'f',
+        age: '3 years old',
+        animalType: 'cat',
+        breedSpecies: 'ragdoll',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Tiger',
+        image:
+        location: 'Wilmington, DE',
+        sex: 'm',
+        age: '5 years old',
+        animalType: 'cat',
+        breedSpecies: 'scottish fold',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Richard',
+        image:
+        location: 'Charlotte, NC',
+        sex: 'm',
+        age: '1 year old',
+        animalType: 'cat',
+        breedSpecies: 'domestic shorthair',
+        neutered: false,
+        description: 
+    },
+    {
+        name: 'Pony',
+        image:
+        location: 'Raleigh, NC',
+        sex: 'm',
+        age: '8 years old',
+        animalType: 'dog',
+        breedSpecies: 'lab mix',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Whiskers',
+        image:
+        location: 'Asheville, NC',
+        sex: 'm',
+        age: '3 years old',
+        animalType: 'cat',
+        breedSpecies: 'ragdoll',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Pawpaw',
+        image:
+        location: 'Asheville, NC',
+        sex: 'm',
+        age: '5 years old',
+        animalType: 'cat',
+        breedSpecies: 'siamese',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Marshall',
+        image:
+        location: 'Raleigh, NC',
+        sex: 'm',
+        age: '10 years old',
+        animalType: 'cat',
+        breedSpecies: 'bengal',
+        neutered: false,
+        description: 
+    },
+    {
+        name: 'Lobo',
+        image:
+        location: 'Charlotte, NC',
+        sex: 'm',
+        age: '6 years old',
+        animalType: 'cat',
+        breedSpecies: 'bengal',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Rasha',
+        image:
+        location: 'Wilmington, DE',
+        sex: 'm',
+        age: '8 years old',
+        animalType: 'cat',
+        breedSpecies: 'domestic shorthair',
+        neutered: true,
+        description: 
+    },
+    {
+        name: 'Raja',
+        image:
+        location: 'Lorton, VA',
+        sex: 'm',
+        age: '2 years old',
+        animalType: 'snake',
+        breedSpecies: 'ball python',
+        neutered: false,
+        description: 
+    },
+    {
+        name: 'Fluffy',
+        image:
+        location: 'Fredricksburg, VA',
+        sex: 'm',
+        age: '2 years old',
+        animalType: 'chameleon',
+        breedSpecies: 'panther chameleon',
+        neutered: false,
+        description: 
+    },
+];
+
+module.exports = petList;
