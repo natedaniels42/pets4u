ca442b997f7e5ffb9d1b4d798bfc5e0f057652a2
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-07-17 13:56:02 -0400

    Fixed dotenv

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pets4us';
-
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
